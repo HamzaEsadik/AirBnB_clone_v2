07281be4b732d7b939806d5bccf83afcb3a4fd55
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-06 22:30:37 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,6 +12,6 @@ sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R "$USER":"$USER" /data/
-loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}"
-sed -i "53i $loc" /etc/nginx/sites-available/default
+loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n\t}"
+sed -i "62i $loc" /etc/nginx/sites-available/default
 sudo service nginx restart
