ca321cf493b2a96962627bfd7923bca731cbe962
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-06 22:41:55 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,7 @@ sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
+sudo echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton school\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R "$USER":"$USER" /data/
 loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n\t}"
