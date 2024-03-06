da25a0d094f5fea332dab413f91740ca39a21937
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-06 23:20:51 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,16 +3,20 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
-sudo service nginx start
 sudo mkdir /data/
 sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton school\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+sudo echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R "$USER":"$USER" /data/
-loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n\t}"
+sudo chown -R ubuntu:ubuntu /data/
+loc="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n\t}"
 sed -i "62i $loc" /etc/nginx/sites-available/default
 sudo service nginx restart
