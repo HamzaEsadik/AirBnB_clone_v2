400b7d39e1bd6fa6b73c7738408d59aec5ac8170
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-06 22:12:02 +0100

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# install and configure nginx
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo service nginx start
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R "$USER":"$USER" /data/
+loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}"
+sed -i "53i $loc" /etc/nginx/sites-available/default
+sudo service nginx restart
