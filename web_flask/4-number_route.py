b4779633e281c1ef34da62b09db2d861982e7423
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-22 12:27:22 +0000

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Base model'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''function that returns Hello HBNB!'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''function that returns HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    '''function that print text variable'''
+    new_text = text.replace('_', ' ')
+    return f'C {new_text}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    '''function that print text variable'''
+    new_text = text.replace('_', ' ')
+    return f'Python {new_text}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    '''display n if is anumber'''
+    if isinstance(int(n), int):
+        return f'{n} is a number'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
