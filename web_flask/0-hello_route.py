711e31afc2f84800b2994d4d44f03f684c48a012
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-22 11:30:20 +0000

    add readme file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''Base model'''
 from flask import Flask
 app = Flask(__name__)
 
@@ -10,4 +11,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
