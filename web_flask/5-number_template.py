95a6d86bb7e2084d824fc4cb10a4538dbd532c33
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-22 12:54:18 +0000

    task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''Base model'''
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''function that returns Hello HBNB!'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''function that returns HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    '''function that print text variable'''
+    new_text = text.replace('_', ' ')
+    return f'C {new_text}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    '''function that print text variable'''
+    new_text = text.replace('_', ' ')
+    return f'Python {new_text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    '''display n if is anumber'''
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def template(n):
+    '''return html page'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
